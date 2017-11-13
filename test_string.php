08f78ede254cf5da2819d2ee3d54de149ba20023
herb123456 <herb123456@gmail.com>
2017-11-13 22:28:29 +0800

    add test_string.php

---
+++ test_string.php
@@ -0,0 +1,50 @@
+<?php
+require_once('./helpers.php');
+
+define('ARRAY_SIZE', 300);
+//define('STR_LENGTH', 100);
+define('TEST_TIME', 100);
+
+$data = [];
+for ($i = 0; $i < ARRAY_SIZE; $i++) {
+    $data[] = generateString(STR_LENGTH);
+}
+
+runTest('serialize', function () use ($data) {
+    for ($i = 0; $i < TEST_TIME; $i++) {
+        serialize($data);
+    }
+});
+
+$sData = serialize($data);
+runTest('unserialize', function () use ($sData) {
+    for ($i = 0; $i < TEST_TIME; $i++) {
+        unserialize($sData);
+    }
+});
+
+runTest('json_encode', function () use ($data) {
+    for ($i = 0; $i < TEST_TIME; $i++) {
+        json_encode($data);
+    }
+});
+
+$sData = json_encode($data);
+runTest('json_decode', function () use ($sData) {
+    for ($i = 0; $i < TEST_TIME; $i++) {
+        json_decode($sData);
+    }
+});
+
+runTest('igbinary_serialize', function () use ($data) {
+    for ($i = 0; $i < TEST_TIME; $i++) {
+        igbinary_serialize($data);
+    }
+});
+
+$sData = igbinary_serialize($data);
+runTest('igbinary_unserialize', function () use ($sData) {
+    for ($i = 0; $i < TEST_TIME; $i++) {
+        igbinary_unserialize($sData);
+    }
+});
